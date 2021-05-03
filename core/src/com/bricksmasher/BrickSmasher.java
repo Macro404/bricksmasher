134e42be99d15b793c9967c40f8e442da2c8e6a4
marwestl <marwestl@kth.se>
2021-05-03 15:09:04 +0200

    Divided game contents into more files. GameScreen.java contains the game functionality, MainMenuScreen.java contains the not yet implemented main menu and BrickSmasher.java serves as an entry point to this. Also added bounce sounds for walls.

--- core/src/com/bricksmasher/BrickSmasher.java
+++ core/src/com/bricksmasher/BrickSmasher.java
@@ -13,109 +13,31 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.Game;
 
-public class BrickSmasher extends ApplicationAdapter {
-	private Texture platformImage;
-	private Texture ballImage;
 
-	private Sound bounceSound;
-	private Sound breakingBlockSound;
-	private Music backgroundMusic;
-	private Rectangle platform;
-	private Rectangle ball;
-	private float xBallSpeed;
-	private float yBallSpeed;
-
-	private SpriteBatch batch;
-	private OrthographicCamera camera;
+public class BrickSmasher extends Game {
+	public SpriteBatch batch;
+	public BitmapFont font;
 	
 	@Override
 	public void create () {
-		platformImage = new Texture(Gdx.files.internal("platform.png"));
-		ballImage = new Texture(Gdx.files.internal("ball.png"));
-
-		bounceSound = Gdx.audio.newSound(Gdx.files.internal("bounceSound.wav"));
-		breakingBlockSound = Gdx.audio.newSound(Gdx.files.internal("breakingBlock.wav"));
-		backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("musicLoop.wav"));
-
-
-		backgroundMusic.setLooping(true);
-		backgroundMusic.play();
-
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-
 		batch = new SpriteBatch();
 
-		platform = new Rectangle();
-		platform.x = 800/2 - 64 / 2;
-		platform.y = 20;
-		platform.width = 96;
-		platform.height = 18;
-
-		xBallSpeed = (-200);
-		yBallSpeed = (-200);
-
-		ball = spawnBall();
+		font = new BitmapFont(); // use libGDX's default Arial font
+		this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0.1f, 0.1f, 0.3f, 1);
-		camera.update();
-
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(platformImage, platform.x, platform.y, platform.width, platform.height);
-		batch.draw(ballImage, ball.x, ball.y, ball.width, ball.width);
-		batch.end();
-
-		move(camera, platform);
-		moveBall();
+		super.render();
 	}
+
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-	}
-
-	private void move(Camera camera, Rectangle platform){
-		if(Gdx.input.isTouched()) {
-			Vector3 touchPos = new Vector3();
-			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			camera.unproject(touchPos);
-			platform.x = touchPos.x - 64 / 2;
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) platform.x -= 400 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) platform.x += 400 * Gdx.graphics.getDeltaTime();
-		if(platform.x < 0) platform.x = 0;
-		if(platform.x > 800 - platform.width) platform.x = 800 - platform.width;
-	}
-
-	private Rectangle spawnBall(){
-		Rectangle ball = new Rectangle();
-
-		ball.width = 12;
-		ball.height = 12;
-		ball.x = 200/2 - ball.width;
-		ball.y = 320;
-		return ball;
-	}
-	private void moveBall(){
-		ball.x += xBallSpeed * Gdx.graphics.getDeltaTime();
-		ball.y += yBallSpeed * Gdx.graphics.getDeltaTime();
-		if(ball.x < 0 || ball.x > 800 - ball.width) {
-			xBallSpeed = -xBallSpeed;
-		}
-		if(ball.overlaps(platform)){
-			bounceSound.play();
-			yBallSpeed = -yBallSpeed;
-		}
-		if(ball.y > 480 - ball.height){
-			yBallSpeed = -yBallSpeed;
-		}
-		if(ball.y + 64 < 0) {
-			ball = spawnBall();
-		}
+		font.dispose();
 	}
 }
