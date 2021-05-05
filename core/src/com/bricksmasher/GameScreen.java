6f538bfe8e263ff6829cd2496d18db009459cbaf
patrikj8 <Patrikj8@kth.se>
2021-05-05 02:19:16 +0200

    Added brick.png

--- core/src/com/bricksmasher/GameScreen.java
+++ core/src/com/bricksmasher/GameScreen.java
@@ -24,6 +24,7 @@ public class GameScreen implements Screen {
     Texture brickImage;
     Sound bounceSound;
     Sound breakingBlockSound;
+    private Array<Rectangle> bricks;
 
     OrthographicCamera camera;
     Rectangle platform;
